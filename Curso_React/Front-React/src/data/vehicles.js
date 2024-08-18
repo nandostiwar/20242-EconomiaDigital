c8a170af3cea642a838c1606a9b3b348dbb9932b
AnderG-Uni <anderson.guevara01@unicatolica.edu.co>
2024-08-17 20:27:37 -0500

    frontend adicione mas data

--- Curso_React/Front-React/src/data/vehicles.js
+++ Curso_React/Front-React/src/data/vehicles.js
@@ -1,38 +1,51 @@
 const vehicles = [
     {
       name: "Car",
-      description: "has four wheels",
+      description: "El automóvil es un vehículo motorizado con ruedas utilizado para el transporte.",
       image:
         "https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d35eacaf1176b0008974b54%2F0x0.jpg%3FcropX1%3D790%26cropX2%3D5350%26cropY1%3D784%26cropY2%3D3349",
     },
     {
       name: "Motorcycle",
-      description: "goes fast",
+      description: "comúnmente conocida en español con la abreviatura moto, es un vehículo de dos ruedas",
       image:
         "https://content2.kawasaki.com/ContentStorage/KMC/Products/8798/8c85b065-f4e9-4ee6-966f-2b7993daff08.png?w=767",
     },
     {
       name: "Bycicle",
-      description: "it's good for the environment",
+      description: "coloquialmente llamada bici​, ​​​es un vehículo de transporte personal de propulsión humana.",
       image:
         "https://media.wired.com/photos/63e569c9de59d567d5d7c66d/master/w_2240,c_limit/Ride1Up-Cafe-Cruiser-Featured-Gear.jpg",
     },
     {
-      name: "Plane",
-      description: "Tickets are expensive",
+      name: "plane",
+      description: "un planeador, es un aerodino de ala fija, con alas de gran envergadura, normalmente carente de motor",
       image: "https://www.rd.com/wp-content/uploads/2020/01/GettyImages-1131335393-e1650030686687.jpg",
     },
     {
       name: "Ship",
-      description: "It is heavy",
+      description: "Barco es todo cuerpo flotante de forma simétrica respecto de un plano longitudinal vertical",
       image: "https://www.marineinsight.com/wp-content/uploads/2019/08/Cruise-ships-1.png",
     },
     {
       name: "Spaceship",
-      description: "Goes to space",
+      description: "Una nave espacial es un vehículo o máquina diseñada para volar en el espacio exterior. ",
       image:
         "https://img.freepik.com/premium-photo/futuristic-scifi-battle-spaceship-hover-ocean-alien-planet-digital-painting_379823-2914.jpg?w=2000",
     },
+    {
+        name: "space shuttle",
+        description: "El Transbordador espacial STS era una nave espacial de la NASA parcialmente reutilizable para órbita baja terrestre",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/STS120LaunchHiRes.jpg/260px-STS120LaunchHiRes.jpg",
+    },
+    {
+        name: "skateboard",
+        description: "El monopatín, también conocido como patineta, patinete, skateboard o tabla de patinaje",
+        image:
+          "https://ichef.bbci.co.uk/ace/ws/624/amz/worldservice/live/assets/images/2015/12/14/151214125329_bbc_instagram_swagway_624x460_thinkstock_nocredit.jpg.webp",
+    },
+
   ];
   
   export default vehicles;
