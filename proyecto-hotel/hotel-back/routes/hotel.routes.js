a7d1dba0dc571b9ab201e26fa32aafe19ace2e1b
MozartCuidadorDigital <45375029+nandostiwar@users.noreply.github.com>
2024-08-24 16:30:29 -0500

    subi el proyecto de calculadora

--- proyecto-hotel/hotel-back/routes/hotel.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const hotelControllers = require('../controllers/hotelControllers.js');
-
-router
-    .get('/', hotelControllers.getAllRooms)
-    .post('/changeStateRoom/:room', hotelControllers.changeState)
-
-module.exports = router;
--- proyecto-hotel/hotel-back/schema/room.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const RoomSchema = new mongoose.Schema({
-    tipo: {type: String, required: true},
-    numero: {type: Number, required: true},
-    Libre: {type: String, required: true}
-})
-
-module.exports = mongoose.model('Room', RoomSchema)
